26a9fc7d08f405b9072457697cc3baf2c23c1763
Samantha Oh <sami_oh@berkeley.edu>
2024-04-22 12:14:21 -0700

    testing injected js

--- assets/injectedScript.js
+++ assets/injectedScript.js
@@ -5,3 +5,11 @@ console.log("hello");
 if (element) {
   element.textContent = "Modified content";
 }
+
+var element2 = document.getElementById("hykaXs");
+console.log(element2.textContent);
+console.log("hello2");
+
+if (element2) {
+  element2.textContent = "Modified content";
+}
