1d661d93635aab3f1adedbf093e12a94e53d2101
Samantha Oh <sami_oh@berkeley.edu>
2024-04-22 22:11:57 -0700

    testing injected js

--- assets/injectedScript.js
+++ assets/injectedScript.js
@@ -18,10 +18,7 @@ var originalOfferHtml1;
 var originalOfferHtml2;
 var originalOfferStyle;
 
-console.log("hello");
-
 // this was a workaround for waiting until the dom is fully loaded before accessing elements
-
 // fires before next repaint
 requestAnimationFrame(() => {
   // fires before the _next_ next repaint
@@ -50,7 +47,6 @@ requestAnimationFrame(() => {
 
 function swapID() {
   console.log("swapping ids");
-
   // need to wait for account from local storage to update
   requestAnimationFrame(() => {
     requestAnimationFrame(() => {
@@ -112,10 +108,12 @@ function changeBalance() {
   balance.style.fontWeight = "800";
 }
 
+// making changes to both offer components
 function changeOffers() {
   var offerDiv = document.getElementsByClassName("fdlpQJ")[0];
   offerDiv.style.flexDirection = "row";
 
+  // using variable named offers referenced global variable, so named this offers1
   var offers1 = document.getElementsByClassName("hDijFX");
   var titleDivs = document.getElementsByClassName("kjJpxs");
 
@@ -203,8 +201,22 @@ function restoreControl() {
   originalOffers[0].lastChild.style.cssText = originalOfferStyle;
 }
 
-// CHALLENGES :')
-// I found out I needed to make changes after the dom has already loaded, this "dom content loaded" function didn't work so my workaround was doing a timeout
+// BUGS + CHALLENGES :')
+
+// 1. You have to refresh the page in order to see the test changes
+// I thought this was due to changing the screen size in dev tools so the application doesn't run again, but when I test it on my mobile phone I also have to refresh
+
+// 2. The application only works correctly when you start on the odd # ID
+// I wasn't sure of what other way to save the original styling and code, other than saving the innerHTML and styles
+// because of this, if we start on an even # ID, it will save the html (and account balance details) for that account
+
+// 3. I wasn't able to get back to the test changes after accepting the offer (you must refresh to get the test changes back)
+// since I don't have access to the id of the "back to homepage" button, I couldn't add an onClick function here to run test changes
+
+// 4. Not sure if this is a bug, but sometimes the first click of "accept offer" leads to a 404 error
+// I didn't change anything on this button except the background color
+
+// 5. I found out I needed to make changes after the dom has already loaded, this "dom content loaded" function didn't work so my workaround was doing a timeout
 // I eventually found a more reliable solution by using the animation frames (there's probably still a better way to do this)
 
 // document.addEventListener("DOMContentLoaded", function () {
